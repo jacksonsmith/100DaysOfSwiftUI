442a71770ae857e4bbd7efa771a02a83d18c372f
jackson <jackson@luanas-MacBook-Air.local>
2024-01-27 16:09:18 -0300

    feat: day 16 creating_a_for

---
+++ 16/01_creating_a_form/form/form/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  form
+//
+//  Created by jackson on 27/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Section {
+                Text("Hello, world!")
+            }
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 16/01_creating_a_form/form/form/formApp.swift
@@ -0,0 +1,17 @@
+//
+//  formApp.swift
+//  form
+//
+//  Created by jackson on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct formApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
